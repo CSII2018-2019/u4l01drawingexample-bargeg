6f7f61ccec04dffe01bb4a3a535fd61d713c6640
gennabarge <gennabarge@10.0.0.10>
2019-03-22 11:32:50 -0400

    Problem with errors

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,13 +1,24 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class DrawingPanel extends JPanel {
 	public DrawingPanel() {
+		setBorder(BorderFactory.createLineBorder(Color.black, 5));
 		
-	}
-	
+}
+	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(250, 200);
 	}
+	
+	DrawingPanel dp = new DrawingPanel();
+	add(dp, BorderLayout.PAGE_END);
+
+
+	
 }
+
+
